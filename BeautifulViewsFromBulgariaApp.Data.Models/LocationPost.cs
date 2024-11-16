f4066b38009b45b3847e189027b6c53aed0a65c3
ELFedrel <ivodzhermanski@gmail.com>
2024-11-16 19:39:16 +0300

    Update LocationPost.cs

--- BeautifulViewsFromBulgariaApp.Data.Models/LocationPost.cs
+++ BeautifulViewsFromBulgariaApp.Data.Models/LocationPost.cs
@@ -1,6 +1,37 @@
-﻿namespace BeautifulViewsFromBulgariaApp.Data.Models
+﻿using static BeautifulViewsFromBulgariaApp.Common.EntityConstants;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautifulViewsFromBulgariaApp.Data.Models
 {
+    using static BeautifulViewsFromBulgariaApp.Common.EntityConstants.LocationPost;
     public class LocationPost
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        [MinLength(TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        [MinLength(DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string PhotoURL { get; set; } = null!;
+
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<FavoriteViews> Favorites { get; set; } = new List<FavoriteViews>();
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
     }
 }
