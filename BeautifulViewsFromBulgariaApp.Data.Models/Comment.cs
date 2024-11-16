6bd540107831de0ac56106cde6bd506cca9991fd
ELFedrel <ivodzhermanski@gmail.com>
2024-11-16 19:30:46 +0300

    Update Comment.cs

--- BeautifulViewsFromBulgariaApp.Data.Models/Comment.cs
+++ BeautifulViewsFromBulgariaApp.Data.Models/Comment.cs
@@ -1,6 +1,37 @@
-﻿namespace BeautifulViewsFromBulgariaApp.Data.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautifulViewsFromBulgariaApp.Data.Models
 {
+    using static BeautifulViewsFromBulgariaApp.Common.EntityConstants.Comment;
     public class Comment
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        [MinLength(ContentMinLength)]
+        public string Content { get; set; } = null!;
+
+
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+
+        [Required]
+        public Guid LocationPostId { get; set; }
+
+        [ForeignKey(nameof(LocationPostId))]
+        public LocationPost LocationPost { get; set; } = null!;
+
+
+
+        [Required]
+        public DateTime DateCreated { get; set; }
     }
 }
